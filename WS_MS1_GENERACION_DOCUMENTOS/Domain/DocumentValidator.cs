7e5aeaf6eaf38dfa3fa5d56f7a32ebc0c2faa2cb
Albert-16 <ca_ardoni@unicah.edu>
2025-07-23 21:19:04 -0600

    Estructura base el proyecto

---
+++ WS_MS1_GENERACION_DOCUMENTOS/Domain/DocumentValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public static class DocumentValidator
+    {
+        public static bool IsValidContent(string content)
+        {
+            return !string.IsNullOrWhiteSpace(content) && content.Length <= 10000;
+        }
+    }
+}
---
+++ WS_MS1_GENERACION_DOCUMENTOS/WS_MS1_GENERACION_DOCUMENTOS/WS_MS1_GENERACION_DOCUMENTOS.asmx.cs
@@ -0,0 +1,46 @@
+﻿using Application;
+using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WS_MS1_GENERACION_DOCUMENTOS
+{
+    /// <summary>
+    /// Summary description for WS_MS1_GENERACION_DOCUMENTOS
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WS_MS1_GENERACION_DOCUMENTOS : System.Web.Services.WebService
+    {
+
+        [WebMethod(Description = "Genera un archivo TXT con el contenido proporcionado.")]
+        public string GenerateTxt(string content)
+        {
+            try
+            {
+                string basePath = Server.MapPath("~/TempFiles/");
+
+                // Instanciar el writer desde infraestructura
+                IDocumentWriter writer = new TxtDocumentWriter();
+
+                // Instanciar el caso de uso desde application
+                var useCase = new GenerateTxtUseCase(writer);
+
+                // Ejecutar la generación
+                string filePath = useCase.Execute(content, basePath);
+
+                return $"Archivo generado exitosamente: {filePath}";
+            }
+            catch (Exception ex)
+            {
+                return $"Error al generar el archivo: {ex.Message}";
+            }
+        }
+    }
+}
