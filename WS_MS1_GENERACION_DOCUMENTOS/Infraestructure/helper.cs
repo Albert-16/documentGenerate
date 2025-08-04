2e6ad29e93db7fe31fe03d4b2b3b961b017ae147
Albert-16 <ca_ardoni@unicah.edu>
2025-08-04 16:50:28 -0600

    helper

--- WS_MS1_GENERACION_DOCUMENTOS/Infraestructure/helper.cs
+++ WS_MS1_GENERACION_DOCUMENTOS/Infraestructure/helper.cs
@@ -13,7 +13,7 @@ namespace Infraestructure.Helper
         private XTextFormatter _formatter;
         private PdfPage _currentPage;
         private double _x, _y, _margin, _ancho;
-        private readonly double _altoMaximoPorPagina;
+        private double _altoMaximoPorPagina;
 
         public PdfTextoAutoHelper(PdfDocument document, double margin = 50)
         {
@@ -24,20 +24,71 @@ namespace Infraestructure.Helper
 
         public void DrawParrafo(string texto, XFont font, XBrush brush, double espacioDespues = 20)
         {
-            var lineHeight = font.GetHeight();
-            var lineas = RomperEnLineas(texto, font);
+            double alturaDisponible = _altoMaximoPorPagina - _y;
 
-            foreach (string linea in lineas)
+            // Medir altura total del párrafo
+            var tamañoParrafo = MedirTexto(texto, font, _ancho);
+
+            // Si no cabe completo, dividir en bloques que sí caben
+            if (tamañoParrafo.Height > alturaDisponible)
             {
-                if (_y + lineHeight > _altoMaximoPorPagina)
-                    AgregarPagina();
+                var fragmentos = DividirTextoPorAltura(texto, font, _ancho, alturaDisponible);
 
-                _formatter.DrawString(linea, font, brush, new XRect(_x, _y, _ancho, lineHeight));
-                _y += lineHeight;
+                foreach (var bloque in fragmentos)
+                {
+                    if (_y + MedirTexto(bloque, font, _ancho).Height > _altoMaximoPorPagina)
+                        AgregarPagina();
+
+                    _formatter.DrawString(bloque, font, brush, new XRect(_x, _y, _ancho, _altoMaximoPorPagina - _y));
+                    _y += MedirTexto(bloque, font, _ancho).Height + espacioDespues;
+                }
+            }
+            else
+            {
+                _formatter.DrawString(texto, font, brush, new XRect(_x, _y, _ancho, tamañoParrafo.Height));
+                _y += tamañoParrafo.Height + espacioDespues;
+            }
+        }
+
+        private XSize MedirTexto(string texto, XFont font, double ancho)
+        {
+            var tf = new XTextFormatter(_gfx);
+            var dummyRect = new XRect(0, 0, ancho, double.MaxValue);
+            tf.DrawString(texto, font, XBrushes.Transparent, dummyRect); // invisible
+            return _gfx.MeasureString(texto, font);
+        }
+
+        private List<string> DividirTextoPorAltura(string texto, XFont font, double ancho, double alturaDisponible)
+        {
+            var lineas = texto.Split(new[] { '\n' }, StringSplitOptions.None);
+            var bloques = new List<string>();
+            string bloqueActual = "";
+            double alturaActual = 0;
+
+            foreach (var linea in lineas)
+            {
+                string nuevoBloque = string.IsNullOrEmpty(bloqueActual) ? linea : bloqueActual + "\n" + linea;
+                var altura = MedirTexto(nuevoBloque, font, ancho).Height;
+
+                if (altura > alturaDisponible)
+                {
+                    bloques.Add(bloqueActual);
+                    bloqueActual = linea;
+                    alturaActual = MedirTexto(linea, font, ancho).Height;
+                }
+                else
+                {
+                    bloqueActual = nuevoBloque;
+                    alturaActual = altura;
+                }
             }
 
-            _y += espacioDespues;
+            if (!string.IsNullOrWhiteSpace(bloqueActual))
+                bloques.Add(bloqueActual);
+
+            return bloques;
         }
+
 
         public void DrawTitulo(string texto, XFont font, XBrush brush, XStringFormat formato, double alto = 30, double espacioDespues = 20)
         {
